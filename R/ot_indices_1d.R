c75268fec421103d73154bb02e320949bc90f0f8
pietrocipolla <leonardo.chiani@polimi.it>
2024-04-08 15:45:43 +0200

    Reorganization

--- R/ot_indices_1d.R
+++ R/ot_indices_1d.R
@@ -73,3 +73,17 @@ ot_indices_1d <- function(x, y, M, extended_out = FALSE) {
 
   return(out)
 }
+
+optimal_transport_1d <- function(partition, y_sort, yc) {
+  # Set the scaling parameters
+  N <- length(y_sort)
+  Nc <- length(yc)
+
+  # Expand the empirical CDF quantiles to match with y1 length
+  yc <- yc[floor(seq(1 / Nc, 1, length.out = N) * Nc + 0.5)]
+
+  # Evaluate the L2 norm of the empirical CDF
+  W <- mean((y_sort - yc)^2)
+
+  return(W)
+}
--- R/ot_indices_bw.R
+++ R/ot_indices_bw.R
@@ -99,3 +99,19 @@ ot_indices_wb <- function(x, y, M, extended_out = FALSE) {
 
   return(out)
 }
+
+optimal_trasport_bw <- function(partition, y, my, Cy, traceCy, Ry) {
+  # Get the current partition indices for y
+  yc <- y[partition, ]
+
+  # Get the conditioned statistics
+  mc <- colMeans(yc)
+  Cc <- stats::cov(yc)
+
+  Adv <- sum((my - mc)^2)
+  Diff <- traceCy + sum(diag(Cc)) - 2 * tracesqrtm(Ry %*% Cc %*% Ry)
+
+  W <- Adv + Diff
+
+  return(cbind(W, Adv, Diff))
+}
